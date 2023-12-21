a758cdf8b0b0a14af486fc4bdadfb348465c1a67
lahaina <nattolecats@gmail.com>
2023-12-21 20:21:55 +0900

    pantah: Import kittens

--- device-blaze.mk
+++ device-blaze.mk
@@ -173,3 +173,5 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-V1-ndk.vendor:64 \
     com.google.hardware.pixel.display-V4-ndk.vendor:64 \
     com.google.hardware.pixel.display-V9-ndk.vendor
+
+-include vendor/ntl/config/kitten.mk
